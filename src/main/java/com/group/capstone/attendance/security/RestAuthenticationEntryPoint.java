89375e2093cce3714d260e087032a4054fc42891
phieulong <phieulong97@gmail.com>
2020-01-15 12:59:25 +0700

    yeahhhhhh

--- src/main/java/com/group/capstone/attendance/security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/group/capstone/attendance/security/RestAuthenticationEntryPoint.java
@@ -1,5 +1,10 @@
 package com.group.capstone.attendance.security;
 
+import com.google.gson.Gson;
+import com.group.capstone.attendance.exception.BadRequest;
+import com.group.capstone.attendance.exception.ErrorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -7,16 +12,26 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
+    @Autowired
+    private Gson gson;
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException) throws IOException {
         // Với các request xác thực không thành công, trả về mã lỗi 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED");
+        ErrorResponse errorResponse = new ErrorResponse("You have to login",HttpStatus.BAD_REQUEST);
+
+        String employeeJsonString = this.gson.toJson(errorResponse);
+
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(employeeJsonString);
+        out.flush();
     }
 }
